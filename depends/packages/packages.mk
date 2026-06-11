21b70ec547014cfbfbca55e931b30e315f039dcd
josibake <josibake@protonmail.com>
2026-06-11 06:05:50 +0100

    build: drop vendored libmultiprocess

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -3,6 +3,6 @@ packages:=
 boost_packages = boost
 
 ipc_packages = capnp
-multiprocess_native_packages = native_libmultiprocess native_capnp
+multiprocess_native_packages = native_capnp
 
 usdt_linux_packages=systemtap
